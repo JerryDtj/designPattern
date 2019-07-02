4cbee900d461467bcf1cf2245b8f352990ecfd70
邓天骄 <jiao_snow@126.com>
2019-07-02 08:36:49 +0800

    简单工厂

---
+++ simpleFactory/src/VersionOne.java
@@ -0,0 +1,16 @@
+import com.sun.deploy.uitoolkit.ui.ConsoleHelper;
+
+/**
+ *
+ * 要求用面向对象的语言实现一个计算器控制台程序，要求输入两个数和运算符号，得到结果。
+ * 简单工厂模式：直接开撸篇
+ *
+ * @author Jerry
+ * @Date 2019-07-02 08:20
+ */
+public class VersionOne {
+    public static void main(String[] args) {
+        System.out.println("亲输入数字A");
+        
+    }
+}
