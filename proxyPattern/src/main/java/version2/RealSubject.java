39fbc13e8c03ade4ebbcad35faa5960aa547da15
邓天骄 <jiao_snow@126.com>
2019-07-08 16:46:15 +0800

    代理模式，自认为版

---
+++ proxyPattern/src/main/java/version2/RealSubject.java
@@ -0,0 +1,14 @@
+package version2;
+
+/**
+ * despaction
+ *
+ * @Author: jerry
+ * @DATE: 2019/7/8 16:29
+ **/
+public class RealSubject extends Subject {
+    @Override
+    public void Request() {
+        System.out.println("subject excuext Request method ");
+    }
+}
